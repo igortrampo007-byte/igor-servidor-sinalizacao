605cc7a7e6a9d203ee936b2f85480251f2f0cb8a
igortrampo007-byte <igortrampo007@gmail.com>
2026-07-17 15:37:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const app = express();
 const http = require('http').createServer(app);
 
-// Configuração do Socket.io
 const io = require('socket.io')(http, {
   cors: {
     origin: "*", 
@@ -14,10 +13,9 @@ const io = require('socket.io')(http, {
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Servidor de Sinalização WebRTC está Online! 🟢');
+  res.send('Servidor WebRTC de Alta Performance está Online! 🟢');
 });
 
-// Lógica de Sinalização
 io.on('connection', (socket) => {
   console.log('Novo dispositivo conectado. ID:', socket.id);
 
@@ -25,31 +23,20 @@ io.on('connection', (socket) => {
     socket.join(room);
   });
 
-  // 🟢 NOVOS EVENTOS DE STATUS VISUAL 🟢
-  socket.on('funcionario_online', (data) => {
-    socket.to(data.room).emit('funcionario_online', data);
+  // Eventos de Status Visual
+  socket.on('funcionario_online', (data) => { socket.to(data.room).emit('funcionario_online', data); });
+  socket.on('funcionario_ausente', (data) => { socket.to(data.room).emit('funcionario_ausente', data); });
+  socket.on('funcionario_offline', (data) => { socket.to(data.room).emit('funcionario_offline', data); });
+
+  // 🟢 NOVO: Evento para Mudar a Resolução Dinamicamente
+  socket.on('set_resolution', (data) => {
+    socket.to(data.room).emit('set_resolution', data.resolution, data.idFuncionario);
   });
 
-  socket.on('funcionario_ausente', (data) => {
-    socket.to(data.room).emit('funcionario_ausente', data);
-  });
-
-  socket.on('funcionario_offline', (data) => {
-    socket.to(data.room).emit('funcionario_offline', data);
-  });
-
-  // Eventos de Vídeo (Mantidos)
-  socket.on('offer', (data) => {
-    socket.to(data.room).emit('offer', data.offer, data.idFuncionario);
-  });
-
-  socket.on('answer', (data) => {
-    socket.to(data.room).emit('answer', data.answer, data.idFuncionario);
-  });
-
-  socket.on('ice-candidate', (data) => {
-    socket.to(data.room).emit('ice-candidate', data.candidate, data.idFuncionario);
-  });
+  // Eventos de Vídeo
+  socket.on('offer', (data) => { socket.to(data.room).emit('offer', data.offer, data.idFuncionario); });
+  socket.on('answer', (data) => { socket.to(data.room).emit('answer', data.answer, data.idFuncionario); });
+  socket.on('ice-candidate', (data) => { socket.to(data.room).emit('ice-candidate', data.candidate, data.idFuncionario); });
 
   socket.on('disconnect', () => {
     console.log('Dispositivo desconectado:', socket.id);
